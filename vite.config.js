9be620629b7ff5da96131c6328c46b2b82a39827
Natascha <na7a5cha010@gmail.com>
2023-03-10 00:06:21 -0400

    Naty-S/issue5: Add Docker (#7)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite";
 import path from "path";
 
 
-/** @type {import('vite').UserConfig} */
+/** @type {import("vite").UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
-			$api: path.resolve('./src/routes/api'),
+			$api: path.resolve("./src/routes/api"),
 		}
+	},
+	server: {
+		strictPort: true,
+		// watch: {
+		// 	usePolling: process.env.USE_POLLING,
+		// },
+		hmr: {
+			clientPort: 3000
+		},
+		host: '0.0.0.0',
+		port: 3000,
 	}
 };
 
